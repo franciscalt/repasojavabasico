2682bee0889fa2332dc483e1bfd887a62ba162ad
francisca Lathrop <franciscalathrop@MacBook-Air-de-Fran.local>
2017-04-05 19:35:29 -0300

    javanuevo

---
+++ main.js
@@ -0,0 +1,10 @@
+var numerouno = prompt("ingresa un numero")
+var numerodos = prompt("ingresa otro numero")
+
+var suma = Number(numerouno) + Number(numerodos)
+var resta = Number(numerouno) - Number(numerodos)
+var multriplica = Number(numerouno) * Number(numerodos)
+var divide = Number(numerouno) / Number(numerodos)
+
+alert("Hola " + ",la suma es " + suma + " ,la resta es " + resta + " ,la multriplicación es " + multriplica + " y la división es " + divide + ".") 
+
